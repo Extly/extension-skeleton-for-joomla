e3a2cfd208abbbdf9037a50d74ebe09396a7c19a
anibalsanchez <anibal.sanchez@extly.com>
2019-02-18 16:50:29 +0100

    babel update

--- build/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-## test -d app/ && npm run production --prefix app/
-## test -d app/ && cp app/dist/* library/media/app/dist/
-
-npm run build
--- build/lib_rebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-LIB_FOLDER="library"
-VENDOR_LIB_FOLDER="library/vendor"
-
-composer update -d $LIB_FOLDER
-### composer update --no-dev -d $LIB_FOLDER
-
-composer dumpautoload --optimize -d $LIB_FOLDER
