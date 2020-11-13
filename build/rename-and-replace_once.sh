4f0d1ef89ee9fa56c519dc901181dc4391250118
anibalsanchez <anibal.sanchez@extly.com>
2020-11-13 17:57:35 +0100

    refactor(lib-composer): Move composer.json to the root folder

--- build/rename-and-replace_once.sh
+++ build/rename-and-replace_once.sh
@@ -8,6 +8,17 @@
 # - Open replace_once_folder.sh, and edit/customize the variables (copy-paste from the .env file)
 # - From the project root, execute build/rename-and-replace_once.sh
 
+EXTENSION_NAME="The New Name for Joomla"
+EXTENSION_ALIAS="thenewname"
+EXTENSION_CLASS_NAME="TheNewName"
+TRANSLATION_KEY="THE_NEW_NAME"
+EXTENSION_DESC="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempus dolor at purus auctor, eget porta justo tempor. Vivamus finibus facilisis eros, at fringilla dolor dictum vitae."
+EXTENSION_NAMESPACE="TheNewNamespace"
+
+sed -i "s/\[EXTENSION_ALIAS\]/$EXTENSION_ALIAS/g" composer.json
+sed -i "s/\[EXTENSION_DESC\]/$EXTENSION_DESC/g" composer.json
+sed -i "s/\[EXTENSION_NAMESPACE\]/$EXTENSION_NAMESPACE/g" composer.json
+
 # Replace
 test -d component && build/replace_once_folder.sh component
 test -d file && build/replace_once_folder.sh file
